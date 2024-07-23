a9129f69d479ba94399011c41aba34eed889fb95
Muhammad Haziq Bin Che Rose <149445613+HAZCHEM234@users.noreply.github.com>
2024-07-24 07:47:23 +1200

    Update app.py
    change the host at the very bottom of the code

--- app.py
+++ app.py
@@ -106,4 +106,5 @@ app.layout = html.Div(style={'backgroundColor': '#1f2c56', 'fontFamily': 'Arial,
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
